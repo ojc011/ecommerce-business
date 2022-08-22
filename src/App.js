48dfd2074954b4c806c562b1bfdf2eb7847d0ec1
Oliver C <ojc011@yahoo.com>
2022-08-22 17:06:12 -0500

    Created routes for home page and shop section, created navigation component for navbar, included a logo which acts as a button to the homepage, and styled the new components accordingly

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 
+import Navigation from './components/navigation/navigation.component';
 import Home from './components/routes/home/home.component';
+import Shop from './components/routes/shop.component';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes> /* Root route to homepage */
   );
 };
