fa3cee639868aa7b5aa57538980a5e4b8e2a36f6
Oliver C <ojc011@yahoo.com>
2022-08-22 16:19:42 -0500

    Created homepage component that will render everything as it already is, added browserrouter to indexjs, changed appjs to accurately reflect the new homepage routing, + npm i react-router-dom

---
+++ src/components/routes/home/home.component.jsx
@@ -0,0 +1,34 @@
+import CategoryIndex from "../../category-index/category-index.component";
+
+const Home = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'PH Category1',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+    },
+    {
+      id: 2,
+      title: 'PH Category2',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+    },
+    {
+      id: 3,
+      title: 'PH Category3',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+    },
+    {
+      id: 4,
+      title: 'PH Category4',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+    },
+    {
+      id: 5,
+      title: 'PH Category5',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+    },
+  ];
+  return <CategoryIndex categories={categories} />;
+};
+
+export default Home;
