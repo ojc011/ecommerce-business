b34afc5f5bb0dddf1bc242597f837e1f2036b542
Oliver C <ojc011@yahoo.com>
2022-08-26 12:42:44 -0500

    Wrapped app in ProductsProvider and stored shop-data in  products context so the data is more easily reachable on a different node.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'; // For navigating between page
 
 import App from './App';
 import { UserProvider } from './contexts/user.context';
+import { ProductsProvider } from './contexts/products.context';
 
 import './index.scss';
 
@@ -12,7 +13,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
